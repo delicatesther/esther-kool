6df16d5ea669130822d55fe1bfd8249983788c07
Esther Kool <kool.esther@gmail.com>
2022-06-28 16:40:49 +0200

    Style business card

--- frontend/components/BusinessCard/BusinessCard.tsx
+++ frontend/components/BusinessCard/BusinessCard.tsx
@@ -6,14 +6,14 @@ export const BusinessCard = ({ src, size }) => {
   return (
     <article className={style.card}>
       <Avatar className={style.circle} src={src} size={size} alt="" />
-      <h2>I'm:</h2>
+      <h2>I'm...</h2>
       <ul className={style.list}>
         <li>Esther Kool (She/Her)</li>
         <li>
           <a href="https://www.twitter.com/delicatesther">@delicatesther</a>
         </li>
-        <li>a front-end developer</li>
-        <li>a D&D nerd</li>
+        <li>a front-end developer from NL 🇳🇱</li>
+        <li>a D&D nerd who prints and paints her own minis</li>
         <li>a lover of music, film, photography & cheese</li>
       </ul>
     </article>
