adb67869352aaacc850e559b7d328d3bc368defa
Esther Kool <kool.esther@gmail.com>
2023-07-11 23:13:43 +0200

    Add wall triangle, colour fixes

--- frontend/components/Layout/Layout.tsx
+++ frontend/components/Layout/Layout.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { Header } from "@enk/components/Header";
-
+import Triangle from "/public/images/triangle-bg.svg";
 export const Layout = ({ children }) => {
   return (
+    <>
     <div className="wrapper">
+      <div className="triangle">
+        <div className="triangleBg">
+          <Triangle role="presentation" />
+        </div>
+      </div>
       <Header />
       <main className="main">
         <div className="grid-wrapper">{children}</div>
       </main>
       <footer className="footer">&copy; Esther Kool {new Date().getFullYear()}</footer>
     </div>
+    </>
   );
 };
