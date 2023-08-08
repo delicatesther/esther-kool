87d456853fc774ffb80c190f69775eefb63e7fad
Esther Kool <e.n.kool@hva.nl>
2023-08-08 12:52:29 +0200

    Add experience detail page WIP

--- frontend/components/Button/Button.tsx
+++ frontend/components/Button/Button.tsx
@@ -5,15 +5,28 @@ import style from "./button.module.scss";
 
 const cx = classNames.bind(style);
 
-export const Button = ({ text, className, onClick, size = "large", icon, ...props }: ButtonProps) => {
-  return (
-    <button {...props} className={cx(["button"], [size], [className])} onClick={onClick}>
-      {text && (
-        <span className={cx(["text"])} data-text={text}>
-          {text}
-        </span>
-      )}
-      {icon && icon}
-    </button>
-  );
+export const Button = ({
+	text,
+	className,
+	onClick,
+	size = "large",
+	icon,
+	iconLeft,
+	...props
+}: ButtonProps) => {
+	return (
+		<button
+			{...props}
+			className={cx(["button"], [size], [className])}
+			onClick={onClick}
+		>
+			{iconLeft && iconLeft}
+			{text && (
+				<span className={cx(["text"])} data-text={text}>
+					{text}
+				</span>
+			)}
+			{icon && icon}
+		</button>
+	);
 };
