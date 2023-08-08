fa8e8463e2d1e49ea5eb208e9cda3e062af9dcf0
Esther Kool <e.n.kool@hva.nl>
2023-08-08 10:16:25 +0200

    Onnodige import

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,25 +1,23 @@
 import { BusinessCard } from "@enk/components/BusinessCard/BusinessCard";
-import { Experiences } from "@enk/components/Experiences";
 import { Layout } from "@enk/components/Layout";
 import { Timeline } from "@enk/components/Timeline";
 import Head from "next/head";
 
 export default function Home() {
-
-  return (
-    <div>
-      <Head>
-        <title>Esther Kool.com</title>
-        <meta name="description" content="Personal website to Esther Kool" />
-      </Head>
-      <Layout>
-        <div className="row">
-          <BusinessCard src="/images/me.jpg" size="large" />
-        </div>
-        <div className="row">
-          <Timeline />
-        </div>
-      </Layout>
-    </div>
-  );
+	return (
+		<div>
+			<Head>
+				<title>Esther Kool.com</title>
+				<meta name="description" content="Personal website to Esther Kool" />
+			</Head>
+			<Layout>
+				<div className="row">
+					<BusinessCard src="/images/me.jpg" size="large" />
+				</div>
+				<div className="row">
+					<Timeline />
+				</div>
+			</Layout>
+		</div>
+	);
 }
