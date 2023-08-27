1bdd5aee273dce2ff188fd43fb969f0fc474b2be
Esther Kool <kool.esther@gmail.com>
2023-08-27 22:37:30 +0200

    Lets try this hacky thing then

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,28 +2,39 @@
 const baseConfig = require("./webpack.common.js");
 
 const nextConfig = {
-  reactStrictMode: true,
-  poweredByHeader: false,
-  publicRuntimeConfig: {
-    GRAPHQL_ENDPOINT: process.env.GRAPHQL_ENDPOINT,
-  },
-  webpack(config, options) {
-    baseConfig,
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: [
-          {
-            loader: "@svgr/webpack",
-            options: {
-              svgo: false,
-              titleProp: true,
-            },
-          },
-        ],
-      });
-    // Important: return the modified config
-    return config;
-  },
+	reactStrictMode: true,
+	poweredByHeader: false,
+	publicRuntimeConfig: {
+		GRAPHQL_ENDPOINT: process.env.GRAPHQL_ENDPOINT,
+	},
+	webpack(config, options) {
+		baseConfig,
+			config.module.rules.push({
+				test: /\.svg$/,
+				use: [
+					{
+						loader: "@svgr/webpack",
+						options: {
+							svgo: false,
+							titleProp: true,
+						},
+					},
+				],
+			});
+		// Important: return the modified config
+		return config;
+	},
+	// Temp workaround that maybe fixes session issue Keystone,
+	// See: https://stackoverflow.com/questions/72297519/keystone-session-cookie-only-working-on-localhost
+	rewrites: () => [
+		{
+			source: "/api/graphql",
+			destination:
+				process.env.NODE_ENV === "development"
+					? `http://localhost:3010/api/graphql`
+					: process.env.GRAPHQL_ENDPOINT,
+		},
+	],
 };
 
 module.exports = nextConfig;
