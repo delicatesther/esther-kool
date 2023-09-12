038c2b7b41ae87ec27b2c25e5cd509a1b7939741
Esther Kool <kool.esther@gmail.com>
2023-09-12 07:19:09 +0200

    Add checklistitem image

---
+++ backend/schemas/CheckListItemImage.ts
@@ -0,0 +1,29 @@
+import "dotenv/config";
+import { relationship, text } from "@keystone-6/core/fields";
+import { list } from "@keystone-6/core";
+import { allowAll } from "@keystone-6/core/access";
+import { cloudinaryImage } from "@keystone-6/cloudinary";
+
+export const cloudinary = {
+  cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+  apiKey: process.env.CLOUDINARY_KEY,
+  apiSecret: process.env.CLOUDINARY_SECRET,
+  folder: "estherkool.com",
+};
+
+export const CheckListItemImage = list({
+  access: allowAll,
+  fields: {
+    image: cloudinaryImage({
+      cloudinary,
+      label: "Source",
+    }),
+    altText: text(),
+    checkListItem: relationship({ ref: "CheckListItem.image" }),
+  },
+  ui: {
+    listView: {
+      initialColumns: ["image", "altText", "product"],
+    },
+  },
+});
--- backend/schemas/User.ts
+++ backend/schemas/User.ts
@@ -1,64 +1,71 @@
 import { Lists } from ".keystone/types";
-import { list, ListConfig } from '@keystone-6/core';
-import { allowAll } from '@keystone-6/core/access';
-import { calendarDay, integer, password, relationship, text, timestamp } from '@keystone-6/core/fields';
+import { list, ListConfig } from "@keystone-6/core";
+import { allowAll } from "@keystone-6/core/access";
+import {
+  calendarDay,
+  integer,
+  password,
+  relationship,
+  text,
+  timestamp,
+} from "@keystone-6/core/fields";
 
 export const Height = list({
-    access: allowAll,
-    fields: {
-        cm: integer({ label: "Height (in cm)" }),
-        createdAt: timestamp({ defaultValue: { kind: 'now' } }),
-    }
+  access: allowAll,
+  fields: {
+    cm: integer({ label: "Height (in cm)" }),
+    createdAt: timestamp({ defaultValue: { kind: "now" } }),
+  },
 });
 
 export const Weight = list({
-    access: allowAll,
-    fields: {
-        g: integer({ label: "Weight (in g)" }),
-        createdAt: timestamp({ defaultValue: { kind: 'now' } }),
-    }
+  access: allowAll,
+  fields: {
+    g: integer({ label: "Weight (in g)" }),
+    createdAt: timestamp({ defaultValue: { kind: "now" } }),
+  },
 });
 
 export const User = list({
-    access: allowAll,
-    fields: {
-        name: text({ validation: { isRequired: true } }),
-        email: text({
-            validation: { isRequired: true },
-            isIndexed: 'unique',
-            isFilterable: true,
-        }),
-        password: password({ validation: { isRequired: true } }),
-        posts: relationship({ ref: 'Post.author', many: true }),
-        experiences: relationship({ ref: 'Experience.author', many: true }),
-        birthdate: calendarDay(),
-        height: relationship({
-            ref: "Height",
-            many: true,
-            ui: {
-                displayMode: "cards",
-                cardFields: ["cm"],
-                inlineCreate: { fields: ["cm"] },
-                inlineEdit: { fields: ["cm"] },
-                inlineConnect: false,
-            },
-        }),
-        weight: relationship({
-            ref: "Weight",
-            many: true,
-            ui: {
-                displayMode: "cards",
-                cardFields: ["g"],
-                inlineCreate: { fields: ["g"] },
-                inlineEdit: { fields: ["g"] },
-                inlineConnect: false,
-            },
-        }),
-
+  access: allowAll,
+  fields: {
+    name: text({ validation: { isRequired: true } }),
+    email: text({
+      validation: { isRequired: true },
+      isIndexed: "unique",
+      isFilterable: true,
+    }),
+    password: password({ validation: { isRequired: true } }),
+    posts: relationship({ ref: "Post.author", many: true }),
+    experiences: relationship({ ref: "Experience.author", many: true }),
+    checkListItems: relationship({ ref: "CheckListItem.users", many: true }),
+    birthdate: calendarDay(),
+    height: relationship({
+      ref: "Height",
+      many: true,
+      ui: {
+        displayMode: "cards",
+        cardFields: ["cm"],
+        inlineCreate: { fields: ["cm"] },
+        inlineEdit: { fields: ["cm"] },
+        inlineConnect: false,
+      },
+    }),
+    weight: relationship({
+      ref: "Weight",
+      many: true,
+      ui: {
+        displayMode: "cards",
+        cardFields: ["g"],
+        inlineCreate: { fields: ["g"] },
+        inlineEdit: { fields: ["g"] },
+        inlineConnect: false,
+      },
+    }),
+  },
+  ui: {
+    listView: {
+      initialColumns: ["name", "posts"],
     },
-    ui: {
-        listView: {
-            initialColumns: ['name', 'posts'],
-        },
-    },
-})
+  },
+});
