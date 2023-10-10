37c0aabd0e67c74b8c35ee8b4a7a04186f46c038
Esther Kool <kool.esther@gmail.com>
2023-10-10 19:17:59 +0200

    Add a README

--- frontend/components/Button/Button.tsx
+++ frontend/components/Button/Button.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { ButtonProps } from "@enk/types";
 import classNames from "classnames/bind";
 import style from "./button.module.scss";
-import Check from "@enk/icons/check.svg";
 const cx = classNames.bind(style);
 
 export const Button = ({
