293b88f0163e46e0f5bb4b55520c929455518e15
Esther Kool <kool.esther@gmail.com>
2023-10-12 20:59:31 +0200

    Clean up business card

--- frontend/components/Avatar/Avatar.tsx
+++ frontend/components/Avatar/Avatar.tsx
@@ -2,26 +2,25 @@ import Image from "next/image";
 import React from "react";
 import classNames from "classnames/bind";
 import style from "./avatar.module.scss";
+import { useTheme } from "next-themes";
 
 const cx = classNames.bind(style);
 
-export const Avatar = ({ className = null, src, size = "medium", alt }) => {
-	// const theme: UseThemeProps = useTheme();
-	// console.log(theme.theme);
-	// const isLight = theme.theme === "light";
+export const Avatar = ({ className = null }) => {
+	const theme = useTheme();
+	const isLight = theme.theme === "light";
 	return (
-		<div className={cx(["avatar"], [size], [className])}>
+		<div className={cx(["avatar"], [className])}>
 			<div className={cx(["frame"])}>
 				<div className={cx(["image"])}>
-					<Image src={src} width={200} height={200} alt={alt} />
+					<Image
+						src={isLight ? "/me-light.jpg" : "/me-dark.jpg"}
+						alt="Esther"
+						width={200}
+						height={200}
+					/>
 				</div>
 			</div>
-			{/* <Image
-          src={isLight ? "/me-light.png" : "/dark-mode-esther.png"}
-          alt="Esther"
-          width={600}
-          height={isLight ? 484 : 443}
-        /> */}
 		</div>
 	);
 };
