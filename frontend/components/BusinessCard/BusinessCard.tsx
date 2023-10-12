e0a93458d44e1a76a58866d635bafbcc28baad02
Esther Kool <kool.esther@gmail.com>
2023-10-12 20:59:31 +0200

    Clean up business card

--- frontend/components/BusinessCard/BusinessCard.tsx
+++ frontend/components/BusinessCard/BusinessCard.tsx
@@ -1,20 +1,18 @@
-import React from "react";
 import classNames from "classnames/bind";
-import style from "./businessCard.module.scss";
-import { Avatar } from "@enk/components/Avatar";
-const cx = classNames.bind(style);
 import { useRouter } from "next/router";
+import { Avatar } from "@enk/components/Avatar";
 import translations from "@enk/translations";
+import style from "./businessCard.module.scss";
 
-export const BusinessCard = ({ src, size }) => {
+const cx = classNames.bind(style);
+
+export const BusinessCard = ({}) => {
 	const router = useRouter();
 	const { locale } = router;
-	const SampleText =
-		"<sample code /> && not true; .some-class { code-goes-here: #bada55; } Once upon a time in a far away place, the dragon before dreams a Dumbo jazz. The wand quite ate a couple big Ginger Bread Men. A few castles in a bewildered ogre terribly said a proud Dumbo. Dumbos of a forest never walk a couple proud dragons. Ginger Bread Men never run those fast Cinderellas. Both Snow Whites happily find a dazzling tower, and they lived happily ever after.";
 	const dictionary = translations[locale].businessCard;
 	return (
 		<article className={style.card}>
-			<Avatar src={src} size={size} alt="" />
+			<Avatar />
 			<h2>
 				{dictionary.title}
 				<span className="visuallyhidden">
