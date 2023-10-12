650e71d3d7b03ae3392028778a26cadaa5595b05
Esther Kool <kool.esther@gmail.com>
2023-10-12 21:00:46 +0200

    Clean up lib

--- frontend/lib/useForm.tsx
+++ frontend/lib/useForm.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 export const useForm = (initial = {}): any => {
 	const [inputs, setInputs] = useState(initial);
--- frontend/lib/utils/ThemeChanger.tsx
+++ frontend/lib/utils/ThemeChanger.tsx
@@ -1,14 +1,13 @@
-import React from "react";
 import { useTheme } from "next-themes";
 import { useEffect } from "react";
 
 export const ThemeChanger = ({ theme }: { theme: "light" | "dark" }) => {
-  const { setTheme } = useTheme();
+	const { setTheme } = useTheme();
 
-  useEffect(() => {
-    console.log(theme, "theme");
-    setTheme(theme);
-  }, [setTheme, theme]);
+	useEffect(() => {
+		console.log(theme, "theme");
+		setTheme(theme);
+	}, [setTheme, theme]);
 
-  return null;
+	return null;
 };
